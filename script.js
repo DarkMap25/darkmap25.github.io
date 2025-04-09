3f70e3c4c96af8794690c84ec42dbc4b3838033c
DarkMap25 <clemmdub@gmail.com>
2025-04-10 01:19:37 +0200

    Update script.js
    1.3

--- script.js
+++ script.js
@@ -6,10 +6,9 @@ const map = L.map('map', {
 }).setView([46.5, 2.5], 6);
 
 // Fond de carte sombre
-L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}.png', {
-  attribution: '&copy; OpenStreetMap & CartoDB',
-  subdomains: 'abcd',
-  maxZoom: 19
+L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png', {
+  attribution: '&copy; Stadia Maps, OpenMapTiles & OpenStreetMap contributors',
+  maxZoom: 20
 }).addTo(map);
 
 // Emoji par catégorie
