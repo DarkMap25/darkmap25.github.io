3e186c29e4e3523242775bf4b3e8de56cc0ce0de
DarkMap25 <clemmdub@gmail.com>
2025-05-22 11:27:14 +0200

    Update script.js

--- script.js
+++ script.js
@@ -17,8 +17,8 @@ const thunderforestAtlasLayer = L.tileLayer('https://tile.thunderforest.com/atla
 
 // Limites de la France métropolitaine + Corse
 const franceBounds = L.latLngBounds(
-  L.latLng(41, -6),
-  L.latLng(55, 11)
+  L.latLng(39, -7),
+  L.latLng(56, 12)
 );
 
 // Initialisation de la carte
